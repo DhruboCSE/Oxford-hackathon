5227c4563a4f565c8cb003a6993983f2034ac30d
Kassim Hussein <kassim.kh@gmail.com>
2018-04-22 10:49:43 -0400

    First commit

---
+++ app/config/constants.js
@@ -0,0 +1,10 @@
+export const APP_NAME = 'Yorkdale Rewards';
+
+export const FIREBASE_API_KEY = "AIzaSyDHWfY7LFZOrRrLjGjh1ay5b0jmeT_nXRE";
+export const FIREBASE_AUTH_DOMAIN = "yorkdalerewards.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://yorkdalerewards.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "yorkdalerewards";
+export const FIREBASE_STORAGE_BUCKET = "yorkdalerewards.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "206206137724";
+
+export const FACEBOOK_APP_ID = '1461511430643147';
